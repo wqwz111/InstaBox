dffa640e585cabd1cd18c7311f13f3b8fa55f8fe
wqwz111 <wqwz111@gmail.com>
2017-07-01 20:21:33 +0800

    New Feat: 1. Add notification to show app status. 2. Download service can be stopped from notification action.

--- app/src/main/java/personal/leo/instabox/Constants.java
+++ app/src/main/java/personal/leo/instabox/Constants.java
@@ -2,7 +2,15 @@ package personal.leo.instabox;
 
 
 public final class Constants {
-    public static final String BROADCAST_ACTION = "personal.leo.instabox.BROADCAST";
+    public static final String BROADCAST_ACTION =
+            "personal.leo.instabox.BROADCAST";
+    public static final String BROADCAST_ACTION_SHOW_DOWNLOAD_BUTTON =
+            "personal.leo.instabox.BROADCAST_SHOW_DOWNLOAD_BUTTON";
+    public static final String BROADCAST_ACTION_RELOAD_MEDIA =
+            "personal.leo.instabox.BROADCAST_RELOAD_MEDIA";
+    public static final String BROADCAST_ACTION_STOP_SERVICE =
+            "personal.leo.instabox.BROADCAST_ACTION_STOP_SERVICE";
+
     public static final String MEDIA_INFO = "personal.leo.instabox.MEDIA_INFO";
     public static final String MEDIA_NAME = "personal.leo.instabox.MEDIA_NAME";
     public static final String MEDIA_URL = "personal.leo.instabox.MEDIA_URL";
